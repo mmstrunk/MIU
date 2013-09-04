f882ed237596e15bda54f320b8e95db9d7eebdce
Michael <mmstrunk@me.com>
2013-09-04 17:40:13 -0400

    20 Json Items

--- Week 1/JS/json.js
+++ Week 1/JS/json.js
@@ -12,7 +12,7 @@ var json = {
 			"fName":	["First Name", "Michael"],
 			"lName":	["Last Name", "Strunk"],
 			"quantity":	["Quantity:", "400"],
-			"details":	["Details:", "Good service is what i base my restaurant visits too."]	
+			"details":	["Details:", "Good service is what i base my restaurant visits on."]	
 			
 		},
 		"Item2": {
@@ -41,6 +41,261 @@ var json = {
 			"lName":	["Last Name", "Strunk"],
 			"quantity":	["Quantity:", "800"],
 			"details":	["Details:", "I love outbacks Green Beens"]	
+			
+		},
+		
+		"Item4": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Arbys"],
+			"favRest":	["What is your Favorite Restaurant:", "Red Robbin"],
+			"mostVis": ["Most visited Restaurant:", "Red Robbin"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "15%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Super"],
+			"lName":	["Last Name", "Man"],
+			"quantity":	["Quantity:", "1400"],
+			"details":	["Details:", "I eat too much fastfood."]	
+			
+		},
+		
+		"Item5": {
+			"visited": ["Late Time Visited Restaurant:", "24 hours"],
+			"lastName": ["Name of last visited Restaurant:", "Red lobster"],
+			"favRest":	["What is your Favorite Restaurant:", "Outback"],
+			"mostVis": ["Most visited Restaurant:", "Mcdonalds"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "20%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Bat"],
+			"lName":	["Last Name", "Man"],
+			"quantity":	["Quantity:", "1000"],
+			"details":	["Details:", "Im Batman"]	
+			
+		},
+		
+		"Item6": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Stonewood"],
+			"favRest":	["What is your Favorite Restaurant:", "Burns Steakhouse"],
+			"mostVis": ["Most visited Restaurant:", "Stonewood"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "22%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Scroog"],
+			"lName":	["Last Name", "McDuck"],
+			"quantity":	["Quantity:", "4000"],
+			"details":	["Details:", "The more i drink the more i tip"]	
+			
+		},
+		
+		"Item7": {
+			"visited": ["Late Time Visited Restaurant:", "3-4 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Burger King"],
+			"favRest":	["What is your Favorite Restaurant:", "Wendys"],
+			"mostVis": ["Most visited Restaurant:", "Wendys"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "10%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "College"],
+			"lName":	["Last Name", "Student"],
+			"quantity":	["Quantity:", "240"],
+			"details":	["Details:", "Dont normally have to tip where i go"]	
+			
+		},
+		
+		"Item8": {
+			"visited": ["Late Time Visited Restaurant:", "Last Week"],
+			"lastName": ["Name of last visited Restaurant:", "Sonic"],
+			"favRest":	["What is your Favorite Restaurant:", "Oyster Bar"],
+			"mostVis": ["Most visited Restaurant:", "Garden Palace"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Tim"],
+			"lName":	["Last Name", "Tebow"],
+			"quantity":	["Quantity:", "800"],
+			"details":	["Details:", ""]	
+			
+		},
+		
+		"Item9": {
+			"visited": ["Late Time Visited Restaurant:", "Last week"],
+			"lastName": ["Name of last visited Restaurant:", "Mcdonalds"],
+			"favRest":	["What is your Favorite Restaurant:", "Olive Garden"],
+			"mostVis": ["Most visited Restaurant:", "Apple Bees"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "15%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Spider"],
+			"lName":	["Last Name", "Man"],
+			"quantity":	["Quantity:", "1500"],
+			"details":	["Details:", "I love me some pasta"]	
+			
+		},
+		
+		"Item10": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "CoCo Joes"],
+			"favRest":	["What is your Favorite Restaurant:", "Sweet barrys"],
+			"mostVis": ["Most visited Restaurant:", "Dairy Queen"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "10%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Sweet"],
+			"lName":	["Last Name", "Tooth"],
+			"quantity":	["Quantity:", "400"],
+			"details":	["Details:", "I only eat out for desert"]	
+			
+		},
+		
+		"Item11": {
+			"visited": ["Late Time Visited Restaurant:", "3-4 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Mcdonalds"],
+			"favRest":	["What is your Favorite Restaurant:", "Mcdonalds"],
+			"mostVis": ["Most visited Restaurant:", "Mcdonalds"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "0%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Ronald"],
+			"lName":	["Last Name", "Mcdonald"],
+			"quantity":	["Quantity:", "999"],
+			"details":	["Details:", "Super size it!"]	
+			
+		},
+		
+		"Item12": {
+			"visited": ["Late Time Visited Restaurant:", "yesterday"],
+			"lastName": ["Name of last visited Restaurant:", "KFC"],
+			"favRest":	["What is your Favorite Restaurant:", "Crab Trap"],
+			"mostVis": ["Most visited Restaurant:", "Burger King"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Wonder"],
+			"lName":	["Last Name", "Women"],
+			"quantity":	["Quantity:", "1800"],
+			"details":	["Details:", "I prefer mcdonalds but BK is closer"]	
+			
+		},
+		
+		"Item13": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Subway"],
+			"favRest":	["What is your Favorite Restaurant:", "Subway"],
+			"mostVis": ["Most visited Restaurant:", "Subway"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "5%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Jared"],
+			"lName":	["Last Name", "Subway"],
+			"quantity":	["Quantity:", "1100"],
+			"details":	["Details:", "Eat Fresh"]	
+			
+		},
+		
+		"Item14": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Mcdonalds"],
+			"favRest":	["What is your Favorite Restaurant:", "Apple Bees"],
+			"mostVis": ["Most visited Restaurant:", "Apple Bees"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "20%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Cat"],
+			"lName":	["Last Name", "Women"],
+			"quantity":	["Quantity:", "2000"],
+			"details":	["Details:", ""]	
+			
+		},
+		
+		"Item15": {
+			"visited": ["Late Time Visited Restaurant:", "5-7 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Red Lobster"],
+			"favRest":	["What is your Favorite Restaurant:", "Sushi Haven"],
+			"mostVis": ["Most visited Restaurant:", "Chick Fil A"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Josh"],
+			"lName":	["Last Name", "Freeman"],
+			"quantity":	["Quantity:", "1500"],
+			"details":	["Details:", "Treat myself after games!"]	
+			
+		},
+		
+		"Item16": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Subway"],
+			"favRest":	["What is your Favorite Restaurant:", "Jersey Mikes"],
+			"mostVis": ["Most visited Restaurant:", "Subway"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Tom"],
+			"lName":	["Last Name", "Jerry"],
+			"quantity":	["Quantity:", "600"],
+			"details":	["Details:", "Im a sub guy"]	
+			
+		},
+		
+		"Item17": {
+			"visited": ["Late Time Visited Restaurant:", "Last month"],
+			"lastName": ["Name of last visited Restaurant:", "Melting pot"],
+			"favRest":	["What is your Favorite Restaurant:", "Fogo"],
+			"mostVis": ["Most visited Restaurant:", "Melting pot"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "30%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Bill"],
+			"lName":	["Last Name", "Gates"],
+			"quantity":	["Quantity:", "5000"],
+			"details":	["Details:", "Good food and service go hand and hand."]	
+			
+		},
+		
+		"Item18": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Popeyes"],
+			"favRest":	["What is your Favorite Restaurant:", "Toco Bell"],
+			"mostVis": ["Most visited Restaurant:", "Chipolta"],
+			"alcohol":	["Do you order Alcohol beverages?", "No"],
+			"tip":	["General % tip", "10%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Alexander"],
+			"lName":	["Last Name", "Bell"],
+			"quantity":	["Quantity:", "1000"],
+			"details":	["Details:", "Ring Ring"]	
+			
+		},
+		
+		"Item19": {
+			"visited": ["Late Time Visited Restaurant:", "3-4 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Mcdonalds"],
+			"favRest":	["What is your Favorite Restaurant:", "Apple Bees"],
+			"mostVis": ["Most visited Restaurant:", "Carabas"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Adrian"],
+			"lName":	["Last Name", "Peterson"],
+			"quantity":	["Quantity:", "2500"],
+			"details":	["Details:", "Quick and easy"]	
+			
+		},
+		
+		"Item20": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Apple Bees"],
+			"favRest":	["What is your Favorite Restaurant:", "Melting Pot"],
+			"mostVis": ["Most visited Restaurant:", "Apple Bees"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Issac"],
+			"lName":	["Last Name", "Nuton"],
+			"quantity":	["Quantity:", "4000"],
+			"details":	["Details:", "I drink alot when i go out."]	
 			
 		},
 
